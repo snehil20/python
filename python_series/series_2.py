f9423078e8a26dbe95a07d9b2886fd818a5d4447
snehil20 <80628056+snehil20@users.noreply.github.com>
2021-03-24 07:07:37 -0700

    Create series_2.py

---
+++ python_series/series_2.py
@@ -0,0 +1,11 @@
+# for series 1,4,7,10......
+a=1
+b=4
+print(a)                                               
+print(b)
+for i in range(19):
+   c=b+3                     
+   a=c                 #assigning values of a and b  w.r.t  c
+   b=a+3
+   print(a)
+   print(b)
